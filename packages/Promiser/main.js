fb5975783f223d7c73e4385d6858279664e38937
Barney Carroll <barney.carroll@barneycarroll.com>
2018-03-25 21:49:42 +0100

    Promiser
    ES5

---
+++ packages/Promiser/main.js
@@ -0,0 +1,41 @@
+var Promise = {
+  view: function(){
+    return v.children[0].children(Object.assign({}, this))
+  },
+  
+  oninit: function(v){
+    this.pending = true
+    
+    v.attrs.promise.then(
+      function(value){
+        this.value = value
+        this.resolved = true
+        this.pending = false
+
+        m.redraw()
+      },
+
+
+      function(error){
+        this.error = error
+        this.rejected = true
+        this.pending = false
+
+        m.redraw()
+      }
+  },
+  
+  onbeforeupdate: function(now, then){
+    if(now.attrs.promise !== then.attrs.promise){
+      for(const key in this)
+        delete this[key]
+        
+      Promise.oninit.apply(this, now)
+    }
+  },
+}
+
+try {
+  module.exports = Promiser
+}
+catch(e){}
