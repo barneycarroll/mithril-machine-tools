0686d788c51094733a7bc4265bf203b3bd2ae4f1
Barney Carroll <barney.carroll@gmail.com>
2020-05-03 13:32:11 +0100

    Created Static > Live proponent
    For static trees with live subtrees!

---
+++ src/Static.js
@@ -0,0 +1,66 @@
+import {viewOf} from './utils.js'
+
+export default function Static(){
+  const ranges = new Map
+  
+  return {
+    view: v =>
+      viewOf(v)(Live),
+      
+    onbeforeupdate: () => {
+      for(const [root, vnodes] of ranges)
+        m.render(root, vnodes.map(copy))
+      
+      return false
+    },
+  }
+  
+  function Live(){
+    let parent
+    let index
+    let range
+    
+    return {
+      view: v =>
+        viewOf(v)(() => {
+          m.render(parent, zone.map(copy))
+        }),
+      
+      oncreate: v => {
+        const {dom} = v
+
+        parent = dom.parentNode
+        index  = indexOf(dom)
+        range  = getSet(ranges, parent, () => 
+          Array(index + 1),
+        )
+
+        range[index] = v
+
+        parent.vnodes = range
+      },
+    }
+  }
+}
+
+function copy(x){
+  return typeof x === 'object' ? Object.assign(new x.constructor, x) : x
+}
+
+function indexOf(dom, index = 0){
+  while(dom = dom.previousSibling)
+    index++
+  
+  return index
+}
+
+function getSet(map, key, factory){
+  if(map.has(key))
+    return map.get(key)
+  
+  const value = factory()
+  
+  map.set(key, value)
+  
+  return value
+}
